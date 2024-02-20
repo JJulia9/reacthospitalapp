0670abdc4903e7c1bad76c9ae3d56c563725f09a
JJulia9 <127937966+JJulia9@users.noreply.github.com>
2024-02-20 14:51:52 +0000

    Initial commit

---
+++ src/pages/shared/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-500"> Register</h1>
+    </>
+  );
+};
+
+export default Register;
